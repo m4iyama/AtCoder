f6aa201f908be6d8c1c856477637e41d66d6c46e
muiscript <myam0118@gmail.com>
2019-04-09 17:14:14 +0900

    add 123

---
+++ 123/d.py
@@ -0,0 +1,14 @@
+import queue
+
+x, y, z, k = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+c = list(map(int, input().split()))
+
+
+q = queue.PriorityQueue()
+
+sumi = x + y + z - 3
+while True:
+
+    q.put(())
